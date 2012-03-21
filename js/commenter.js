fa5a8f5cee1ac4993e99bb12b8cd4edd035c6772
Leonard Techel <mail@leonardtechel.de>
2012-03-21 21:57:40 +0100

    Switched from prompt to jQueryUI-Dialog; Support for easy-to-color notes.

--- js/commenter.js
+++ js/commenter.js
@@ -1,9 +1,60 @@
 function Commenter(json_source) {
+	var pos_x, pos_y;
+
 	// create a new layer
 	var frame = $('body').append('<div id="comments_layer"></div>');
 	$('#comments_layer').append('<div id="comments_innerlayer"></div>');
 	$('#comments_layer').append('<canvas id="comments_static_canvas" class="comments_canvas" width="' + $(window).width() + '" height="' + $(window).height() + '"></canvas>');
 	$('#comments_layer').append('<canvas id="comments_dynamic_canvas" class="comments_canvas" width="' + $(window).width() + '" height="' + $(window).height() + '"></canvas>');
+
+	// create the modal
+	$('#comments_layer').append('<div id="comments_modal" title="Create a new comment"><form><input placeholder="Name" type="text" id="comment_add_name" /><textarea placeholder="Comment" id="comment_add_comment"></textarea></form><h4>Choose a color:</h4><div id="comments_add_color_chooser"><div id="comment_add_color_red"></div><div id="comment_add_color_green"></div><div id="comment_add_color_blue"></div></div><div id="comment_add_color_field">foobar</div></div>');
+	//// the color chooser
+	$('#comment_add_color_field').css('background', '#' + rgbToHex([125, 125, 125]));
+	$('#comment_add_color_red, #comment_add_color_green, #comment_add_color_blue').slider({
+		orientation: 'vertical',
+		max:	255,
+		value:	125,
+		range:	'min',
+		slide:	function() {
+			var r = $('#comment_add_color_red').slider('value');
+			var g = $('#comment_add_color_green').slider('value');
+			var b = $('#comment_add_color_blue').slider('value');
+			// check for dark colors to set the right font color
+			if((r + g + b) < 300) {
+				$('#comment_add_color_field').css('color', '#fff');
+			} else {
+				$('#comment_add_color_field').css('color', '#000');
+			}
+			// set the color-show-div-background
+			$('#comment_add_color_field').css('background', '#' + rgbToHex([r, g, b]));
+		}
+	});
+	//// the modal itself
+	$('#comments_modal').dialog({
+		autoOpen:	false,
+		buttons: {
+			'Create the comment': function() {
+				// get the color
+				var r = $('#comment_add_color_red').slider('value');
+				var g = $('#comment_add_color_green').slider('value');
+				var b = $('#comment_add_color_blue').slider('value');
+
+				// create the comment
+				userAddComment(
+					$('#comment_add_name').val(),
+					$('#comment_add_comment').val(),
+					'#' + rgbToHex([r, g, b]),
+					[pos_x, pos_y]
+				);
+
+				$(this).dialog('close');
+			},
+			'Cancel': function() {
+				$(this).dialog('close');
+			}
+		}
+	});
 
 	// create something that resizes the canvas on window resize
 	/*
@@ -14,7 +65,10 @@ function Commenter(json_source) {
 
 	// create the comment generator
 	$('#comments_innerlayer').click(function(e) {
-			userAddComment([e.pageX, e.pageY]);
+		pos_x = e.pageX;
+		pos_y = e.pageY;
+			//userAddComment([e.pageX, e.pageY]);
+		$('#comments_modal').dialog('open');
 	});
 
 	// create the canvas handlers
@@ -37,6 +91,7 @@ function Commenter(json_source) {
 	this.userAddComment = userAddComment;
 	this.loadComments = loadComments;
 	this.redrawLines = redrawLines;
+	this.rgbToHex = rgbToHex;
 
 	function hide() {
 		$('#comments_layer').fadeOut(500);
@@ -46,6 +101,20 @@ function Commenter(json_source) {
 	}
 	function toggle() {
 		$('#comments_layer').fadeToggle(500);
+	}
+	function rgbToHex(rgb) {
+		// stolen from http://jqueryui.com/demos/slider/#colorpicker
+		var hex = [
+			rgb[0].toString(16),
+			rgb[1].toString(16),
+			rgb[2].toString(16)
+		];
+		$.each(hex, function(nr, val) {
+			if (val.length === 1) {
+				hex[nr] = '0' + val;
+			}
+		});
+		return hex.join('').toUpperCase();
 	}
 	function redrawLines() {
 		// clear the canvas
@@ -70,6 +139,7 @@ function Commenter(json_source) {
 						json[v]['name'],
 						json[v]['comment'],
 						json[v]['time'],
+						json[v]['color'],
 						json[v]['position']
 					);
 				});
@@ -78,7 +148,7 @@ function Commenter(json_source) {
 			}
 		});
 	}
-	function addComment(author, content, time, position) {
+	function addComment(author, content, time, color, position) {
 		// create the static canvas points
 		if(static_canvas.getContext) {
 			static_ctx.beginPath();
@@ -94,7 +164,7 @@ function Commenter(json_source) {
 		var r_time = date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "." + date.getMonth() + "." + date.getFullYear();
 
 		// create the comment
-		$('#comments_innerlayer').append('<div style="left:' + (parseInt(position[0]) + 15) + 'px;top:' + (parseInt(position[1]) + 15) + 'px;" class="comment" id="' + time + '">' + author + '<br>' + content + '<br>' + r_time + '</div>')
+		$('#comments_innerlayer').append('<div style="left:' + (parseInt(position[0]) + 15) + 'px;top:' + (parseInt(position[1]) + 15) + 'px;background:' + color + ';" class="comment" id="' + time + '">' + author + '<br>' + content + '<br>' + r_time + '</div>')
 		
 		// fade it in
 		$('#' + time).fadeIn(1000);
@@ -121,70 +191,72 @@ function Commenter(json_source) {
 	function addCloseLink(elem) {
 		$('#comments_layer').append('<a id="comment_close" href="javascript:' + elem + '.hide();">X</a>')
 	}
-	function userAddComment(position) {
-		var user = prompt('Your Name:');
-		if(user != null) {
-			var comment = prompt('Your Comment:');
-			if(comment != null) {
-				// generate the new json
-				var time = Math.round((new Date()).getTime() / 1000);
-				var newJson = {
-					'name': user,
-					'comment': comment,
-					'position': position,
-					'time': time
-				};
-				oldJson.push(newJson);
+	function userAddComment(user, comment, color, position) {
+		// reload the comments to prevent overwriting
+		loadComments();
 
-				// lock the file
-				var lockToken;
-				data = '<?xml version="1.0" ?><D:lockinfo xmlns:D="DAV:"><D:lockscope><D:shared /></D:lockscope><D:locktype><D:write/></D:locktype></D:lockinfo>';
-				$.ajax({
-					type:		'LOCK',
-					headers:	{'Timeout': 'Second-1000'},
-					url:		json_source,
-					async:		false,
-					data:		data,
-					dataType:	'xml',
-					success:	function(text) {
-						// get the lock token
-						lockToken = $(text).find('href').text();
-					}/*,
-					complete:	function(xhr, text) {
-						// check for a lock
-						if(xhr.status != 423) {
-							// deal with the existing lock. remember: it's only a second long
-						}
-					}*/
-				});
-				console.log('locked');
+		// generate the new json
+		var time = Math.round((new Date()).getTime() / 1000);
+		var newJson = {
+			'name':		user,
+			'comment':	comment,
+			'position':	position,
+			'color':	color,
+			'time':		time
+		};
+		oldJson.push(newJson);
 
-				// get the latest version
-				loadComments();
+		// lock the file
+		var lockToken;
+		data = '<?xml version="1.0" ?><D:lockinfo xmlns:D="DAV:"><D:lockscope><D:shared /></D:lockscope><D:locktype><D:write/></D:locktype></D:lockinfo>';
+		$.ajax({
+			type:		'LOCK',
+			headers:	{'Timeout': 'Second-1000'},
+			url:		json_source,
+			async:		false,
+			data:		data,
+			dataType:	'xml',
+			success:	function(text) {
+				// get the lock token
+				lockToken = $(text).find('href').text();
+			}/*,
+			complete:	function(xhr, text) {
+				// check for a lock
+				if(xhr.status != 423) {
+					// deal with the existing lock. remember: it's only a second long
+				}
+			}*/
+		});
+		console.log('locked');
 
-				// push it to the server
-				$.ajax({
-					type:		'PUT',
-					headers:	{'If': '(<' + lockToken + '>)'},
-					url:		json_source,
-					async:		false,
-					data:		JSON.stringify(oldJson)
-				});
-				console.log('putted');
+		// get the latest version
+		loadComments();
 
-				// unlock the file
-				$.ajax({
-					type:		'UNLOCK',
-					headers:	{'Lock-Token': '<' + lockToken + '>'},
-					url:		json_source,
-					async:		false,
-					complete:	function () {
-						// add the comment
-						addComment(user, comment, time, position);
-					}
-				});
-				console.log('unlocked');
+		// push it to the server
+		$.ajax({
+			type:		'PUT',
+			headers:	{'If': '(<' + lockToken + '>)'},
+			url:		json_source,
+			async:		false,
+			data:		JSON.stringify(oldJson)
+		});
+		console.log('putted');
+
+		// unlock the file
+		$.ajax({
+			type:		'UNLOCK',
+			headers:	{'Lock-Token': '<' + lockToken + '>'},
+			url:		json_source,
+			async:		false,
+			complete:	function (xhr) {
+				// add the comment if everything went well
+				if(String(xhr.status).substr(0, 2) == "20") {
+					addComment(user, comment, time, color, position);
+				} else {
+					alert("Ooups, something went wrong!");
+				}
 			}
-		}
+		});
+		console.log('unlocked');
 	}
 }
